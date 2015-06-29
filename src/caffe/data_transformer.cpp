5eb296c7587e286611df09d10215e35571bf5cf4
dangweili <dangweili@gmail.com>
2015-06-29 13:26:49 +0800

    add implement for multidata's transform in transform.cpp

--- src/caffe/data_transformer.cpp
+++ src/caffe/data_transformer.cpp
@@ -416,6 +416,135 @@ int DataTransformer<Dtype>::Rand(int n) {
   return ((*rng)() % n);
 }
 
+// -------------support for the multidata ----------------
+template<typename Dtype>
+void DataTransformer<Dtype>::Transform(const MultiDatum& datum,
+                                        Blob<Dtype>* transformed_blob) {
+  const int datum_channels = datum.channels();
+  const int datum_height = datum.height();
+  const int datum_width = datum.width();
+
+  const int channels = transformed_blob->channels();
+  const int height = transformed_blob->height();
+  const int width = transformed_blob->width();
+  const int num = transformed_blob->num();
+
+  CHECK_EQ(channels, datum_channels);
+  CHECK_LE(height, datum_height);
+  CHECK_LE(width, datum_width);
+  CHECK_GE(num, 1);
+
+  const int crop_size = param_.crop_size();
+
+  if (crop_size) {
+    CHECK_EQ(crop_size, height);
+    CHECK_EQ(crop_size, width);
+  } else {
+    CHECK_EQ(datum_height, height);
+    CHECK_EQ(datum_width, width);
+  }
+  
+  Dtype* transformed_data = transformed_blob->mutable_cpu_data();
+  Transform( datum, transformed_data);
+
+}
+
+// ------process transform-------------------
+// ------we just process color images for convernent--------
+// one color image has three channels, this should be checked.
+// image_mean should be a N*3 channel image
+template<typename Dtype>
+void DataTransformer<Dtype>::Transform(const MultiDatum& datum,
+                                       Dtype* transformed_data) {
+  const string& data = datum.data();
+  const int datum_channels = datum.channels();
+  const int datum_height = datum.height();
+  const int datum_width = datum.width();
+
+  const int crop_size = param_.crop_size();
+  const Dtype scale = param_.scale();
+  const bool do_mirror = param_.mirror() && Rand(2);
+  const bool has_mean_file = param_.has_mean_file();
+  const bool has_uint8 = data.size() > 0;
+  const bool has_mean_values = mean_values_.size() > 0;
+    
+  // ----------get the image cnt and label cnt-----------------
+  const int img_cnt = param_.img_cnt();
+  const int label_cnt = param.label_cnt();
+
+
+  CHECK_GT(datum_channels, 0);
+  CHECK_GE(datum_height, crop_size);
+  CHECK_GE(datum_width, crop_size);
+
+  Dtype* mean = NULL;
+  if (has_mean_file) {
+    // ----ignore the channels count-------------
+    CHECK_EQ(datum_channels, data_mean_.channels() * img_cnt);
+    CHECK_EQ(datum_height, data_mean_.height());
+    CHECK_EQ(datum_width, data_mean_.width());
+    mean = data_mean_.mutable_cpu_data();
+  }
+
+  if (has_mean_values) {
+    CHECK(mean_values_.size() == 1 || mean_values_.size() == datum_channels/img_cnt ) <<
+     "Specify either 1 mean_value or as many as channels: " << datum_channels;
+    if (datum_channels > 1 && mean_values_.size() == 1) {
+      for (int c = 1; c < datum_channels; ++c) {
+        mean_values_.push_back(mean_values_[0]);
+      }
+    }
+  }
+
+  int height = datum_height;
+  int width = datum_width;
+  int h_off = 0;
+  int w_off = 0;
+  if (crop_size) {
+    height = crop_size;
+    width = crop_size;
+    if (phase_ == TRAIN) {
+      h_off = Rand(datum_height - crop_size + 1);
+      w_off = Rand(datum_width - crop_size + 1);
+    } else {
+      h_off = (datum_height - crop_size) / 2; // using the center part
+      w_off = (datum_width - crop_size) / 2;
+    }
+  }
+  // -----------compute off of an image---------------------
+  Dtype datum_element;
+  int top_index, data_index;
+  for (int c = 0; c < datum_channels; ++c) {
+    for (int h = 0; h < height; ++h) {
+      for (int w = 0; w < width; ++w) {
+        data_index = (c * datum_height + h_off + h) * datum_width + w_off + w;
+        if (do_mirror) {
+          top_index = (c * height + h) * width + (width - 1 - w);
+        } else {
+          top_index = (c * height + h) * width + w;
+        }
+        if (has_uint8) {
+          datum_element =
+            static_cast<Dtype>(static_cast<uint8_t>(data[data_index]));
+        } else {
+          datum_element = datum.float_data(data_index);
+        }
+        if (has_mean_file) {
+          transformed_data[top_index] =
+            (datum_element - mean[data_index]) * scale;
+        } else {
+          if (has_mean_values) {
+            transformed_data[top_index] =
+              (datum_element - mean_values_[c]) * scale;
+          } else {
+            transformed_data[top_index] = datum_element * scale;
+          }
+        }
+      }
+    }
+  } // end for channel
+}
+
 INSTANTIATE_CLASS(DataTransformer);
 
 }  // namespace caffe
