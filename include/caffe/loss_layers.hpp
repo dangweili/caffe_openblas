b1d29b5c5c8cfb7641a42d163a70dddb3e9db5d3
dangweili <dangweili@gmail.com>
2015-07-27 08:53:49 +0800

    first version of tripletloss

--- include/caffe/loss_layers.hpp
+++ include/caffe/loss_layers.hpp
@@ -765,6 +765,49 @@ class SoftmaxWithLossLayer : public LossLayer<Dtype> {
   int softmax_axis_, outer_num_, inner_num_;
 };
 
+// ----------------triplet loss layer -------------------------------
+// reference to tangwei
+template <typename Dtype>
+class TripletLossLayer: public LossLayer<Dtype> {
+    public:
+        explicit TripletLossLayer(const LayerParameter& param)
+            : LossLayer<Dtype>(param) {}
+        virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom, 
+            const vector<Blob<Dtype>*>& top);
+        virtual inline int ExactNumBottomBlobs() const { return 3; }
+        virtual inline const char* type() const { return "TripletLoss"; }
+        /*
+            just backward one layer
+        */ 
+        virtual inline bool AllowForceBackward(const int bottom_index) const {
+            return bottom_index != 3;
+        }      
+    protected:
+        virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom, 
+                const vector<Blob<Dtype>*>& top);
+        virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+                const vector<Blob<Dtype>*>& top);
+
+        virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+                const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+        virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+                const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+        Blob<Dtype> diff_ap_; // cache for backward pass
+        Blob<Dtype> diff_an_; // cache for backward pass
+        Blob<Dtype> diff_pn_; // cache for backward pass
+        
+        Blob<Dtype> diff_sq_ap_; // cache for backward pass
+        Blob<Dtype> diff_sq_an_; // cache for backward pass
+        
+        Blob<Dtype> dist_sq_ap_; // cache for backward pass
+        Blob<Dtype> dist_sq_an_; // cache for backward pass
+        
+        Blob<Dtype> summer_vec_; // tmp storage for gpu forward pass
+        Blob<Dtype> dist_binary_; // tmp storage for gpu forward pass
+};
+
 }  // namespace caffe
 
 #endif  // CAFFE_LOSS_LAYERS_HPP_
