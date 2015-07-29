0c8bf6374e3f695c4e38c191578e259da822dc5e
dangweili <dangweili@gmail.com>
2015-07-29 23:24:48 +0800

    add support for L2 Normalize

--- include/caffe/common_layers.hpp
+++ include/caffe/common_layers.hpp
@@ -459,6 +459,36 @@ class SliceLayer : public Layer<Dtype> {
   vector<int> slice_point_;
 };
 
+/***
+ * --------add support for L2 Normalization Layer----------------
+***/
+template <typename Dtype>
+class NormalizeLayer : public Layer<Dtype> {
+    public: 
+        explicit NormalizeLayer( const LayerParameter& param)
+            : Layer<Dtype>(param) {}
+        /*virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+            const vector<Blob<Dtype>*>& top);
+        */
+        virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+            const vector<Blob<Dtype>*>& top);
+        virtual inline const char* type() const { return "Normalize"; }
+        virtual inline int ExactNumBottomBlobs() const { return 1; }
+        virtual inline int ExactNumTopBlobs() const {return 1;}
+        
+    protected:
+        virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+            const vector<Blob<Dtype>*>& top);
+        virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+            const vector<Blob<Dtype>*>& top);
+        virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+            const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+        virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+            const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+        Blob<Dtype> squared_;
+};
+
 }  // namespace caffe
 
 #endif  // CAFFE_COMMON_LAYERS_HPP_
